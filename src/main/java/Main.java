bca7d439bada2bc5abdf7994df2f68d0cca8f4e1
Marcel <marcel.behar@gmail.com>
2016-09-18 23:04:30 +0200

    Methods Implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main implements DS1Interface  {
 
@@ -12,19 +13,102 @@ public class Main implements DS1Interface  {
 
     @Override
     public int[] insertionSort(int[] input) {
-        return new int[0];
+    	for (int i = 1; i <= input.length - 1; i++){
+    		int key = input[i];
+    		int j = i-1;
+    		while (j >= 0 && input[j] > key){
+    			input[j+1] = input[j];
+    			j = j-1;
+    		input[j+1] = key;
+    		}
+    	}
+        return input;
     }
-
 
     @Override
     public int[] mergeSort(int[] input) {
-        return new int[0];
+    	if (input.length <=1){
+    		return input;
+    	}
+    	int q = Math.floorDiv(input.length, 2);
+    	int[] left = Arrays.copyOfRange(input, 0, q);
+    	int[] right = Arrays.copyOfRange(input, q, input.length);
+    	left = mergeSort(left);
+    	right = mergeSort(right);
+    	return merge(left, right);
     }
-
+    
+    private int[] merge(int[] left, int[] right) {
+    	int i = 0;
+    	int j = 0;
+    	int[] result = new int[left.length+right.length];
+    	while (i < left.length && j < right.length){
+    		if (left[i] <= right[j]){
+    			result[i+j] = left[i];
+    			i++;
+    		}
+    		else{
+    			result[i+j] = right[j];
+    			j++;
+    		}
+    	}
+    	if (i == left.length && j < right.length){
+    		while (j < right.length){
+    			result[i+j] = right[j];
+    			j++;
+    		}
+    	}
+    	else if (j == right.length && i < left.length){
+    		while (i < left.length){
+    			result[i+j] = left[i];
+    			i++;
+    		}
+    	}
+    	return result;
+    }
 
     @Override
     public int[] heapSort(int[] input) {
-        return new int[0];
+    	buildHeap(input);
+    	for (int i = n; i > 0; i--){
+    		swap(input,0,i);
+    		n--;
+    		buildMaxHeap(input,0);
+    	}
+        return input;
+    }
+
+    private static int n;
+    
+	private static void swap(int[] input, int i, int i2) {
+		int tmp = input[i];
+        input[i] = input[i2];
+        input[i2] = tmp; 
+	}
+
+	private static void buildHeap(int[] input) {
+		n = input.length-1;
+        for (int i = n/2; i >= 0; i--){
+            buildMaxHeap(input,i);
+        }
+	}
+
+	private static void buildMaxHeap(int[] input, int i) {
+		{ 
+	        int left = 2*i ;
+	        int right = 2*i + 1;
+	        int max = i;
+	        if (left <= n && input[left] > input[i])
+	            max = left;
+	        if (right <= n && input[right] > input[max])        
+	            max = right;
+	 
+	        if (max != i)
+	        {
+	            swap(input, i, max);
+	            buildMaxHeap(input, max);
+	        }
+	    }  
     }
 
 
@@ -95,5 +179,5 @@ public class Main implements DS1Interface  {
         }
 
     }
-
+    
 }
